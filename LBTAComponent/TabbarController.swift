1f6a59861f857d292055621a63869a4a8e44d77b
mteera <chaceteera@chaces-mbp.home>
2017-10-25 21:41:13 +0100

    Fixed issue with disappearing custom tabbar

--- LBTAComponent/TabbarController.swift
+++ LBTAComponent/TabbarController.swift
@@ -17,6 +17,22 @@ class TabbarController: UITabBarController, UITabBarControllerDelegate {
         tabBar.isTranslucent = false
         tabBar.tintColor = UIColor.white
         
+        let tabOne = NewsViewController()
+        let tabOneBarItem = UITabBarItem(title: "News", image: UIImage(named: "megaphone-selected"), selectedImage: UIImage(named: ""))
+        
+        tabOne.tabBarItem = tabOneBarItem
+        
+        
+        let tabFive = HomedatasourceController()
+        let tabFiveBarItem5 = UITabBarItem(title: "User", image: UIImage(named: "user-selected"), selectedImage: UIImage(named: "user-selected"))
+        
+        tabFive.tabBarItem = tabFiveBarItem5
+        self.viewControllers = [tabOne, tabFive]
+        
+        setupMiddleButton()
+
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -27,54 +43,46 @@ class TabbarController: UITabBarController, UITabBarControllerDelegate {
     
     func setupMiddleButton() {
         
-        let menuButton = UIButton(frame: CGRect(x: 0, y: 0, width: 128, height: 64))
+        let menuButton = UIButton(frame: CGRect(x: 0, y: 0, width: 94, height: 64))
         
         var menuButtonFrame = menuButton.frame
         menuButtonFrame.origin.y = view.bounds.height - menuButtonFrame.height
         menuButtonFrame.origin.x = view.bounds.width/2 - menuButtonFrame.size.width/2
         menuButton.frame = menuButtonFrame
-        
         //Styling
         menuButton.backgroundColor = UIColor.darkGray
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
         menuButton.layer.cornerRadius = menuButton.layer.frame.width/2
         menuButton.setImage(UIImage(named: "OS-eye-white-condensed"), for: UIControlState.normal)
         menuButton.addTarget(self, action: #selector(TabbarController.menuButtonAction), for: UIControlEvents.touchUpInside)
         view.addSubview(menuButton)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        let controller1 = NewsViewController(self.view.backgroundColor = UIColor.white)
+        controller1.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarSystemItem.contacts, tag: 1)
+        let nav1 = UINavigationController(rootViewController: controller1)
         
-        setupMiddleButton()
-        let tabOne = NewsViewController()
-        let tabOneBarItem = UITabBarItem(title: "News", image: UIImage(named: "megaphone-selected"), selectedImage: UIImage(named: ""))
+        let controller2 = NewsViewController()
+        controller2.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarSystemItem.contacts, tag: 2)
+        let nav2 = UINavigationController(rootViewController: controller2)
         
-        tabOne.tabBarItem = tabOneBarItem
+        let controller3 = NewsViewController()
+        let nav3 = UINavigationController(rootViewController: controller3)
+        nav3.title = ""
         
+        let controller4 = NewsViewController()
+        controller4.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarSystemItem.contacts, tag: 4)
+        let nav4 = UINavigationController(rootViewController: controller4)
         
-        let tabTwo = NewsViewController()
-        let tabTwoBarItem2 = UITabBarItem(title: "Passed", image: UIImage(named: "thumbs-down-selected"), selectedImage: UIImage(named: ""))
+        let controller5 = HomedatasourceController()
+        controller5.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarSystemItem.contacts, tag: 5)
+        let nav5 = UINavigationController(rootViewController: controller5)
         
-        tabTwo.tabBarItem = tabTwoBarItem2
-        
-        let tabThree = NewsViewController()
-        let tabThreeBarItem3 = UITabBarItem(title: "", image: UIImage(named: "defaultImage2.png"), selectedImage: UIImage(named: "selectedImage2.png"))
-        
-        tabThree.tabBarItem = tabThreeBarItem3
-        
-        let tabFour = NewsViewController()
-        let tabFourBarItem4 = UITabBarItem(title: "Meet", image: UIImage(named: "thumbs-up-selected"), selectedImage: UIImage(named: ""))
-        
-        tabFour.tabBarItem = tabFourBarItem4
-        
-        let tabFive = HomedatasourceController()
-        let tabFiveBarItem5 = UITabBarItem(title: "User", image: UIImage(named: "user-selected"), selectedImage: UIImage(named: "user-selected"))
-        
-        tabFive.tabBarItem = tabFiveBarItem5
-        self.viewControllers = [tabOne, tabTwo, tabThree, tabFour, tabFive]
-        
+        self.viewControllers = [nav1, nav2, nav3, nav4, nav5]
+        self.setupMiddleButton()
     }
-    
+
     func menuButtonAction(sender: UIButton) {
         self.selectedIndex = 2
         // console print to verify the button works
